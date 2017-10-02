88d6c1acd1ad7566e8326b758d66de7912e76609
Steven White <stwhi@microsoft.com>
2017-10-02 12:26:18 -0700

    initial creation

---
+++ Win32ServerCoreMissingFunctions/Editor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+using WDCMLSDK;
+using WDCMLSDKBase;
+
+namespace WDCMLSDKDerived
+{
+	using Win32ServerCoreMissingFunctions;
+
+	/// <summary>
+	/// See the xml docs for EditorBase.
+	/// </summary>
+	internal class Editor : EditorBase
+	{
+		public Editor(FileInfo fileInfo, XNamespace xNamespace = null) : base(fileInfo, xNamespace) { }
+
+		// Methods that don't modify.
+
+		// Methods that modify. Set this.IsDirty to true only you modify the document directly, not
+		// if you call a method that already does so.
+	}
+}
---
+++ Win32ServerCoreMissingFunctions/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+using WDCMLSDK;
+using WDCMLSDKBase;
+
+namespace Win32ServerCoreMissingFunctions
+{
+}
+
+namespace WDCMLSDKDerived
+{
+	using Win32ServerCoreMissingFunctions;
+
+	/// <summary>
+	/// See the xml docs for ProgramBase.
+	/// </summary>
+	internal class Program : ProgramBase
+	{
+		// Logs
+		private Log exceptionLog = null;
+		private Log libraryNamesLog = null;
+
+		// Data
+		//private Dictionary<string, string> uniqueKeyMap = null;
+		private Dictionary<string, List<string>> functionsInDesktopExperienceButNotInServerCore = null;
+
+		static int Main(string[] args)
+		{
+			return (new Program()).Run();
+		}
+
+		protected override void OnRun()
+		{
+			var apiRefModelWin32 = ApiRefModelWin32.GetApiRefModelWin32(Platform.Win32DesktopAndWsuaDotTxt);
+
+			//this.uniqueKeyMap = this.LoadUniqueKeyMap("uniqueKeyMap.txt");
+			this.functionsInDesktopExperienceButNotInServerCore = this.LoadNonUniqueKeyMap("functionsInDesktopExperienceButNotInServerCore.txt");
+			foreach (var binaryName in this.functionsInDesktopExperienceButNotInServerCore.Keys)
+			{
+				foreach (var functionName in this.functionsInDesktopExperienceButNotInServerCore[binaryName])
+				{
+					FunctionWin32InDocs foundFunctionWin32 = null;
+					apiRefModelWin32.GetFunctionWin32ByName(functionName, ref foundFunctionWin32);
+				}
+			}
+		}
+	}
+}
