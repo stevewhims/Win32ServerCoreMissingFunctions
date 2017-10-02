7b003bb2ccef682d79e4c68e3d4d9c0753ceae0d
Steven White <stwhi@microsoft.com>
2017-10-02 13:10:33 -0700

    logs and counts

--- Win32ServerCoreMissingFunctions/Program.cs
+++ Win32ServerCoreMissingFunctions/Program.cs
@@ -20,8 +20,8 @@ namespace WDCMLSDKDerived
 	internal class Program : ProgramBase
 	{
 		// Logs
-		private Log exceptionLog = null;
-		private Log libraryNamesLog = null;
+		private Log notPublishedMissingWin32FunctionsLog = null;
+		private Log publishedMissingWin32FunctionsLog = null;
 
 		// Data
 		//private Dictionary<string, string> uniqueKeyMap = null;
@@ -34,18 +34,56 @@ namespace WDCMLSDKDerived
 
 		protected override void OnRun()
 		{
-			var apiRefModelWin32 = ApiRefModelWin32.GetApiRefModelWin32(Platform.Win32DesktopAndWsuaDotTxt);
+			this.notPublishedMissingWin32FunctionsLog = new Log()
+			{
+				Filename = "NotPublishedMissingWin32Functions_Log.txt",
+				AnnouncementStyle = ConsoleWriteStyle.Default,
+				Headers = new string[] { "binaryName", "functionName" }
+			};
+			this.RegisterLog(this.notPublishedMissingWin32FunctionsLog);
+
+			this.publishedMissingWin32FunctionsLog = new Log()
+			{
+				Filename = "PublishedMissingWin32Functions_Log.txt",
+				AnnouncementStyle = ConsoleWriteStyle.Default,
+				Headers = new string[] { "binaryName", "functionName" }
+			};
+			this.RegisterLog(this.publishedMissingWin32FunctionsLog);
 
 			//this.uniqueKeyMap = this.LoadUniqueKeyMap("uniqueKeyMap.txt");
 			this.functionsInDesktopExperienceButNotInServerCore = this.LoadNonUniqueKeyMap("functionsInDesktopExperienceButNotInServerCore.txt");
+
+			int totalValidMissingWin32FunctionNames = 0;
+			int notPublishedMissingWin32FunctionNames = 0;
+			int publishedMissingWin32FunctionNames = 0;
+
+			var apiRefModelWin32 = ApiRefModelWin32.GetApiRefModelWin32(Platform.Win32DesktopAndWsuaDotTxt);
+
 			foreach (var binaryName in this.functionsInDesktopExperienceButNotInServerCore.Keys)
 			{
+				totalValidMissingWin32FunctionNames += this.functionsInDesktopExperienceButNotInServerCore[binaryName].Count;
+
 				foreach (var functionName in this.functionsInDesktopExperienceButNotInServerCore[binaryName])
 				{
 					FunctionWin32InDocs foundFunctionWin32 = null;
 					apiRefModelWin32.GetFunctionWin32ByName(functionName, ref foundFunctionWin32);
+
+					if (foundFunctionWin32 != null)
+					{
+						++publishedMissingWin32FunctionNames;
+						this.publishedMissingWin32FunctionsLog.AddEntry(binaryName, functionName);
+					}
+					else
+					{
+						++notPublishedMissingWin32FunctionNames;
+						this.notPublishedMissingWin32FunctionsLog.AddEntry(binaryName, functionName);
+					}
 				}
 			}
+
+			ProgramBase.ConsoleWrite(string.Format("{0} valid names in functionsInDesktopExperienceButNotInServerCore.txt", totalValidMissingWin32FunctionNames));
+			ProgramBase.ConsoleWrite(string.Format("{0} of those are published", publishedMissingWin32FunctionNames));
+			ProgramBase.ConsoleWrite(string.Format("{0} are not published", notPublishedMissingWin32FunctionNames));
 		}
 	}
 }
